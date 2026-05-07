c927a9b8af323366f916fffc237ec07918ca1ca6
Kirill <noname14022007@mail.ru>
2026-05-07 22:42:11 +0300

    updated server

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
-
+import dotenv from "dotenv";
+dotenv.config();
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI, {
@@ -12,5 +13,10 @@ const connectDB = async () => {
         process.exit(1);
     }
 };
-
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => console.log("MongoDB connected"))
+    .catch((err) => console.error("MongoDB connection error:", err));
 export default connectDB;
